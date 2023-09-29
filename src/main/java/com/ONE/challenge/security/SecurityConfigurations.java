e1d127dcb77fc1abfc6648f5527854916306a5a1
Ange1D <47231898+Ange1D@users.noreply.github.com>
2023-09-28 19:07:54 -0600

    Documentacion de la API

--- src/main/java/com/ONE/challenge/security/SecurityConfigurations.java
+++ src/main/java/com/ONE/challenge/security/SecurityConfigurations.java
@@ -8,11 +8,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @EnableWebSecurity
@@ -22,18 +25,23 @@ public class SecurityConfigurations {
     private SecurityFilter securityFilter;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.csrf(csrf -> csrf.disable())
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, HandlerMappingIntrospector introspector) throws Exception {
+        MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector);
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests((authorize) ->
-                        authorize.requestMatchers(HttpMethod.POST,"/login")
-                                .permitAll())
-                .authorizeHttpRequests((request -> request.requestMatchers(HttpMethod.POST, "/usuarios")
-                                        .permitAll()
-                                        .anyRequest()
-                                        .authenticated()
+                        //Factory method 'securityFilterChain' threw exception with message: This method cannot decide whether these patterns are Spring MVC patterns or not.
+                        //https://stackoverflow.com/questions/76852537/endpoint-spring-security-configuration-returning-the-method-cannot-decide-whet
+                        authorize.requestMatchers(mvcMatcherBuilder.pattern(HttpMethod.POST,"/login")).permitAll())
+                        .authorizeHttpRequests((request -> request.requestMatchers(mvcMatcherBuilder.pattern(HttpMethod.POST,"/usuarios"))
+                                .permitAll()
+                                .requestMatchers(mvcMatcherBuilder.pattern("/documentacion/swagger-ui.html")).permitAll()
+                                .requestMatchers(mvcMatcherBuilder.pattern("/v3/api-docs/**")).permitAll()
+                                .requestMatchers(mvcMatcherBuilder.pattern("/swagger-ui/**")).permitAll()
+                                .anyRequest()
+                                .authenticated()
                  ))
-                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                //.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
 
                 .build();
     }
